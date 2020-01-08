00c97b613d3470454f7c73694dc5873af3256b3d
Evans Shao <evanzshao@gmail.com>
2020-01-07 17:41:32 -0800

    retry 5

--- frontend/chess.jsx
+++ frontend/chess.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Root from "./components/Root";
+import Root from "./components/root";
 import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,6 +1,6 @@
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import App from "./App";
+import App from "./app";
 import React from "react";
 
 const Root = ({ store }) => (
