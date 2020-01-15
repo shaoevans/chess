406bbc90d47343a4b83b7df356fae7f120186fc9
Evans Shao <evanzshao@gmail.com>
2020-01-14 16:34:48 -0800

    heroku final push hopefully

--- app/channels/game_room_channel.rb
+++ app/channels/game_room_channel.rb
@@ -8,9 +8,9 @@ class GameRoomChannel < ApplicationCable::Channel
     match = Match.find_by(id: data["matchId"])
     old_move_string = match.move_string
     match.update(move_string: old_move_string + " " + data.move)
-    if (data["gameOver"]) {
+    if data["gameOver"]
       match.update(pending: false)
-    }
+    end
     socket = { move: move }
     ChatChannel.broadcast_to('game_room_channel', socket)
   end
