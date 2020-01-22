9a4ab2a3a8cd29bae78182ff5dc1de78ce78dd74
Evans Shao <evanzshao@gmail.com>
2020-01-22 00:11:48 -0800

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = ['https://evanschess.herokuapp.com']
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
+  
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
